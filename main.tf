75dae9c5ca59bfb7c27b4c2f7a9a7731d7805160
David Dobbins <david.dobbins@nerdery.com>
2020-05-28 14:16:07 -0700

    updated teraform

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+provider "azurerm" {
+  # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
+  version = "=2.11.0"
+  features {}
+}
+
 terraform {
   required_version = "> 0.12.0"
 
